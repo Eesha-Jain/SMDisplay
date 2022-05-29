a7d5f7d35564b7e95a06bfb834e8175291e2af34
Eesha-Jain <eesha.jain801@gmail.com>
2022-05-29 12:46:03 -0700

    added requirements.txt

--- smdisplay/settings.py
+++ smdisplay/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+"import django_heroku" 
+"django_heroku.settings(locals())" 
